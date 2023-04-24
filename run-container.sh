836e9ed791e7ed17a4a6c0388bb125a9c02471af
pranavVention <pranav.jain@vention.cc>
2023-04-24 19:48:41 -0400

    added privlages

--- run-container.sh
+++ run-container.sh
@@ -2,8 +2,21 @@
 echo "Removing old container..."
 docker rm  ros2-humble-container
 
+# add non-network local connections to access control list:
+xhost +local:
+
+
 # Run container
 docker  run -it \
+        --privileged \
+        --net=host \
+        --ulimit nofile=1024:524288 \
         --name="ros2-humble-container" \
+        --expose=9091 -p 9091:9091 \
+        --env="DISPLAY" \
+        --env="QT_X11_NO_MITSHM=1" \
+        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        --env=NVIDIA_VISIBLE_DEVICES=all \
+        --env=NVIDIA_DRIVER_CAPABILITIES=all \
         ros2-humble-image\
         zsh
